3b5150deb0f3acdfec0c985ef4760e526385d534
朱恒霖 <zhuhenglin21@gmail.com>
2024-01-04 20:45:22 +0800

    feat: header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors:{
+        primary: '#007AFF',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
