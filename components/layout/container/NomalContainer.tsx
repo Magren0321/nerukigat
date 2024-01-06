1a075e2be36d947e7c223b7ae61f56880a416838
Magren <44224328+Magren0321@users.noreply.github.com>
2024-01-06 12:44:49 +0800

    Update NomalContainer.tsx

--- components/layout/container/NomalContainer.tsx
+++ components/layout/container/NomalContainer.tsx
@@ -6,8 +6,7 @@ export const NormalContainer = ( { children } : {children: React.ReactNode} ) =>
   return (
     <div
       className={clsx(
-        'mx-auto mt-14 max-w-3xl px-2 lg:mt-[80px] lg:px-0 2xl:max-w-4xl',
-        '[&_header.prose]:mb-[80px]',
+        'mx-auto mt-14 max-w-3xl px-2 lg:mt-[80px] lg:px-0 2xl:max-w-4xl mb-[80px]'
       )}
     >
       {children}
