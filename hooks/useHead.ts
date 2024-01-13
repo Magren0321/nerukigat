2373828ebd88ff4946ef623808f77a8dcde81119
Magren <44224328+Magren0321@users.noreply.github.com>
2024-01-13 11:43:23 +0800

    feat: hooks

---
+++ hooks/useHead.ts
@@ -0,0 +1,29 @@
+import React from 'react'
+
+type Headings = {
+  id: string
+  title: string
+  level: number
+}[]
+
+export const useHeadings = () => {
+  const [headings, setHeadings] = React.useState<Headings>([])
+
+  React.useEffect(() => {
+    const elements = Array.from(
+      document.querySelectorAll<HTMLHeadingElement>(
+        "h1,h2,h3"
+      )
+    )
+      .filter((element) => element.id)
+      .map((element) => ({
+        id: element.id,
+        title: element.textContent ?? '',
+        level: Number(element.tagName.substring(1)),
+      }))
+
+    setHeadings(elements)
+  }, [])
+
+  return headings
+}
