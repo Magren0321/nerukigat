ea34122be3f03bf35b5c6504810b8ff3f1ca24ae
朱恒霖 <zhuhenglin21@gmail.com>
2024-01-19 11:11:51 +0800

    feat: post desc

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,16 +6,31 @@ import { NormalContainer } from '@/components/layout/container/NomalContainer'
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+    <Link href={post.url} >
+      <div className="py-10">
+        <h1 className="relative break-words text-2xl mb-6 font-bold">
           {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
-      </time>
-    </div>
+        </h1>
+        <div className='break-all leading-loose text-gray-800/90 dark:text-gray-200/90 mb-3'>
+          {post.description}
+        </div>
+        <div className='flex items-center text-xs text-gray-600 dark:text-gray-200'>
+          <time dateTime={post.date} className='mr-3'>
+            {format(parseISO(post.date), 'LLLL d, yyyy')}
+          </time>
+          <div>
+            # 
+            {post.tags.map((tag, idx) => (
+              <span key={idx}>
+                {tag}
+                {idx < post.tags.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+          </div>
+        </div>
+        
+      </div>
+    </Link>
   )
 }
 
