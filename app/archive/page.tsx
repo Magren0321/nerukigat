55d84a49e0bd9c4651ce11ad2120b51278a47f87
朱恒霖 <zhuhenglin21@gmail.com>
2024-01-22 10:59:23 +0800

    refactor: archive

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,78 +1,29 @@
-import { allPosts, Post } from 'contentlayer/generated'
-import { compareDesc, format, parseISO } from 'date-fns'
-import Link from 'next/link'
+'use client'
+
+import { TimeLine } from '@/components/ui/timeline/TimeLine'
 import { NormalContainer } from "@/components/layout/container/NomalContainer"
+import { useSearchParams } from "next/navigation";
+import { getPostTimeLine } from '@/utils'
 
 export default function Archive() {
+  const searchParams = useSearchParams();
+  const tag = searchParams.get("tag") || '';
 
-  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
-
-  const dateMap = {} as Record<string, Post[]>
-
-  posts.forEach((post) => {
-    const year = format(parseISO(post.date), 'yyyy')
-    if (!dateMap[year]) {
-      dateMap[year] = []
-    }
-    dateMap[year].push(post);
-  })
+  const { value , length } = getPostTimeLine(tag);
 
   const TimeHead = () =>{
     return (
       <header>
-        <h1 className="text-3xl font-bold">归档</h1>
-        <h2 className='text-xl font-bold mt-10'>目前共有{posts.length}篇文章，继续努力</h2>
+        <h1 className="text-3xl font-bold">{ tag || '归档' }</h1>
+        <h2 className='text-xl font-bold mt-10'>目前共有{length}篇文章，继续努力</h2>
       </header>
-    )
-  }
-
-  const TimeLine = () => {
-    return (
-      <div className='mt-10'>
-        {
-          dateMap && Object.keys(dateMap).reverse().map((year, idx) => (
-            <div key={idx} className='mb-10'>
-              <div className='text-xl font-bold'>{year}</div>
-              <div className='mt-5 text-sm'>
-                {
-                  dateMap[year].map((post, idx) => (
-                    <div key={idx} className='mb-5'>
-                      <div className='flex items-center justify-between text-gray-800/90 dark:text-gray-200/90'>
-                        <div className='font-medium relative'>
-                          <time dateTime={post.date} className='mr-3'>
-                            {format(parseISO(post.date), 'MM/dd')}
-                          </time>
-                          <Link href={post.url} className='relative timeline-link'>
-                            <span>{post.title}</span>
-                          </Link>
-                        </div>
-                        <div className='hidden lg:block'>
-                          #
-                          {
-                            post.tags.map((tag, idx) => (
-                              <span key={idx}>
-                                {tag}
-                                {idx < post.tags.length - 1 ? ', ' : ''}
-                              </span>
-                            ))
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  ))
-                }
-              </div>
-            </div>
-          ))  
-        }
-      </div>
     )
   }
 
   return (
     <NormalContainer>
       <TimeHead />
-      <TimeLine />
+      <TimeLine dateMap={value} />
     </NormalContainer>
   )
 }
