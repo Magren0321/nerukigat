e3829ff6ed5a5a1bdae3249338a1808c918aae07
朱恒霖 <zhuhenglin21@gmail.com>
2024-01-26 13:58:52 +0800

    fix: toc

--- components/ui/toc/PostItem.tsx
+++ components/ui/toc/PostItem.tsx
@@ -9,14 +9,24 @@ export const PostItem = ({
   text: string,
   id: string
 }) =>{
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const scrollPosition = element.offsetTop;
+      window.scrollTo({
+        top: scrollPosition,
+        behavior: 'smooth'
+      });
+    }
+  };
   return (
     <>
       <a
+        onClick={() => scrollTo(id)}
         className={`${
           active ? 'text-black dark:text-white' : 'text-gray-400 dark:text-black-200'
         } block overflow-hidden text-ellipsis text-nowrap py-1 cursor-pointer font-sans font-medium hover:text-black dark:hover:text-white`}
         style={{ paddingLeft: `${level}rem` }}
-        href={`#${id}`}
       >
         {text}
       </a>
