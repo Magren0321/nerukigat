0137d9f20b2434e5671fbf6e5335b7bd40b4cf3a
朱恒霖 <zhuhenglin21@gmail.com>
2024-02-05 09:54:29 +0800

    feat: style

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <main>
       <PostContainer>
-        <main className="flex justify-between items-center lg:pt-[110px] pt-[40px] flex-col lg:flex-row">
-          <div className="text-xl font-bold mb-48 lg:mt-0 lg:mb-0 w-full max-w-xl lg:text-3xl">
+        <main className="flex justify-between items-center rounded-lg lg:p-[110px] py-[30px] px-[10px] flex-col lg:flex-row bg-white dark:bg-zinc-700">
+          <div className="text-xl font-bold mb-32 lg:mt-0 lg:mb-0 w-full max-w-xl lg:text-3xl ">
             <div className="mb-5 pl-1">Hi👋🏻, I&#39;m Magren 🦊</div>
             <div className="mb-5 pl-1">很高兴在这见到你</div>
             <TypedText />
