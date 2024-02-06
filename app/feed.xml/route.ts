d3d19f8f40cb4e52583bcc7a77fc5c94c4edec99
朱恒霖 <zhuhenglin21@gmail.com>
2024-02-06 09:29:49 +0800

    feat: rss

---
+++ app/feed.xml/route.ts
@@ -0,0 +1,40 @@
+import RSS from 'rss'
+import { allPosts , Post } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
+
+export interface MetaData{
+  title: string
+	slug: string
+	description: string
+	publish: boolean
+	date: string
+	tags: string[]
+}
+
+export async function GET() {
+  const feed = new RSS({
+    title: `Magren's Blog`,
+    description: '不为繁华易匠心',
+    site_url: 'https://magren.cc', 
+    feed_url: 'https://magren.cc/feed.xml', 
+    language: 'en', // 网站语言代码
+    image_url: 'https://magren.cc/avatar.png',
+  })
+  const data =  allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+
+  data.forEach((post: Post) => {
+    feed.item({
+      title: post.title,
+      guid: post.slug, 
+      url: `https://magren.cc/posts/${post.slug}`,
+      description: post.description || '', 
+      date: new Date(post.date), 
+    })
+  })
+ 
+  return new Response(feed.xml(), {
+    headers: {
+      'content-type': 'application/xml'
+    }
+  })
+}
