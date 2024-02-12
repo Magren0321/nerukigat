383a3e22d71fe73d45f37ad4a5f2205e638515e0
朱恒霖 <zhuhenglin21@gmail.com>
2024-02-12 13:55:35 +0800

    feat: friend

--- app/friends/config.ts
+++ app/friends/config.ts
@@ -5,6 +5,13 @@ interface FriendItem {
   desc: string;
 }
 
-const friendData: FriendItem[] = [];
+const friendData: FriendItem[] = [
+  {
+    name: '7gugu’s Blog',
+    link: 'https://www.7gugu.com/',
+    avatar: 'https://7gugu.com/wp-content/uploads/2021/07/favicon.png',
+    desc: '一只鸽子的Code Space'
+  }
+];
 
 export default friendData;
