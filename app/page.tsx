e39a0dd84d357fbc3102f03db67864fa23f7410b
Magren <zhuhenglin21@gmail.com>
2024-09-30 17:42:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,17 @@ import { PostContainer } from '@/components/layout/container/PostContainer';
 import { Avatar } from '@/components/ui/avatar/Avatar';
 import { SocialList } from '@/components/ui/social/SocialList';
 import { TypedText } from '@/components/ui/typed/TypedText';
+import clsx from 'clsx';
 
 export default function Home() {
   return (
     <main>
       <PostContainer>
-        <main className="flex flex-col items-center justify-between rounded-lg bg-white px-[10px] py-[30px] lg:flex-row lg:p-[110px] dark:bg-zinc-700">
+        <main className={clsx(
+          'flex flex-col items-center justify-between rounded-lg lg:flex-row px-[10px] py-[30px]  lg:p-[110px]  transition-all duration-300  ',
+          'bg-white dark:bg-zinc-700 border-solid border-[1px]',
+          'hover:shadow-[0_0_200px_1px_rgb(37,99,235,0.3)]  hover:border-[rgb(37,99,235,0.7)] hover:scale-[1.01] hover:rotate-[1deg] ',
+        )}>
           <div className="mb-32 w-full max-w-xl text-xl font-bold lg:mb-0 lg:mt-0 lg:text-3xl ">
             <div className="mb-5 pl-1">Hi👋🏻, I&#39;m Magren 🦊</div>
             <div className="mb-5 pl-1">很高兴在这见到你</div>
