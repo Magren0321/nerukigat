2686cdc9c4256c4eef001a7a22cf0c64fb0acc93
Magren <zhuhenglin21@gmail.com>
2024-12-15 23:10:37 +0800

    fix: hover style

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <main className={clsx(
           'flex flex-col items-center justify-between rounded-lg lg:flex-row px-[10px] py-[30px]  lg:p-[110px]  transition-all duration-300  ',
           'bg-white dark:bg-zinc-700 border-solid border-[1px]',
-          'hover:shadow-[0_0_200px_1px_rgb(0,0,0,0.2)] dark:hover:shadow-[0_0_200px_1px_rgb(255,255,255,0.2)] hover:scale-[1.01] ',
+          'hover:scale-105 hover:shadow-[0_0_50px_1px_rgba(0,0,0,0.2)] dark:hover:shadow-[0_0_50px_1px_rgba(255,255,255,0.2)] transition-transform transition-shadow"',
         )}>
           <div className="mb-32 w-full max-w-xl text-xl font-bold lg:mb-0 lg:mt-0 lg:text-3xl ">
             <div className="mb-5 pl-1">Hi👋🏻, I&#39;m Magren 🦊</div>
