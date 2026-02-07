f93972eb704dbe79c467ac37d32656bc5a8c75e0
Magren <zhuhenglin21@gmail.com>
2026-02-07 16:04:57 +0800

    refactor: update PostLayout and PostTree components for improved layout and responsiveness

--- components/ui/toc/PostTree.tsx
+++ components/ui/toc/PostTree.tsx
@@ -7,8 +7,11 @@ import { PostItem } from './PostItem';
 export const PostTree = () => {
   const headings = useHeadings();
   const activeId = useScroll(headings.map((heading) => heading.id));
+
+  if (headings.length === 0) return null;
+
   return (
-    <div className="sticky top-28 hidden h-fit pl-8 text-sm lg:block">
+    <div className="group sticky top-28 hidden h-fit w-[200px] shrink-0 pl-8 text-sm lg:block">
       {headings.map((heading) => (
         <PostItem
           key={heading.id}
